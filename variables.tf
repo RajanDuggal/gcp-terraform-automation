e10f0fae7453820b4776b1ddbf1a1ab494c93b14
rajanduggal <rajanduggal@github.com>
2026-07-13 15:23:55 +0000

    fixing terraform plan stuck at loading

--- variables.tf
+++ variables.tf
@@ -1,26 +1,65 @@
-variable "project_id" {
-  description = "The GCP Project ID where resources will be deployed"
+# ------------------------------------------------------------------------------
+# CORE INFRASTRUCTURE VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "project-f2bce33d-23f5-4831-9b4" {
   type        = string
+  description = "The Google Cloud Project ID"
+  # No default on purpose - this MUST be passed securely via GitHub Actions YAML
 }
 
 variable "gcp_region" {
-  description = "The region to deploy resources into"
   type        = string
-  default     = "us-central1" # If no value is provided, it defaults to this
+  description = "The Google Cloud region to deploy resources in"
+  default     = "us-central1"
+}
+
+variable "gcp_zone" {
+  type        = string
+  description = "The specific zone within the region"
+  default     = "us-central1-a"
 }
 
 variable "environment_prefix" {
-  description = "Prefix for naming resources (e.g., dev, prod, globant-prep)"
   type        = string
-}
-variable "gke_num_nodes" {
-  description = "Number of nodes in the GKE cluster"
-  type        = number
-  default     = 1
+  description = "Prefix for naming resources (e.g., dev, prod, globant-prep)"
+  default     = "globant-prep"
 }
 
-variable "gke_machine_type" {
-  description = "The machine type for GKE nodes"
+# ------------------------------------------------------------------------------
+# NETWORK VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "vpc_name" {
   type        = string
-  default     = "e2-small" # Keeping it small to save costs
+  description = "Name of the Virtual Private Cloud network"
+  default     = "main-vpc"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  description = "CIDR range for the primary subnet"
+  default     = "10.0.0.0/16"
+}
+
+# ------------------------------------------------------------------------------
+# KUBERNETES (GKE) VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the GKE cluster"
+  default     = "primary-cluster"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "The machine type for the GKE nodes"
+  default     = "e2-medium"
+}
+
+variable "node_count" {
+  type        = number
+  description = "Number of worker nodes in the GKE cluster"
+  default     = 1
 }
